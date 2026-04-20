5e4ffb42317fc09a9b573419478c581ad3d36526
webdev68 <webdev@onephuket.co>
2026-04-20 13:49:32 +0700

    modified:   app/api/access/user-accounts/route.js 	modified:   lib/validateApiKey.js

--- app/api/access/user-accounts/route.js
+++ app/api/access/user-accounts/route.js
@@ -8,5 +8,7 @@
     - เหมาะกับระบบ Login, Permission, Access Control
     - ตอนนี้ยังไม่ได้ใส่ validateApiKey เพราะ export มาจาก admin route ตรง ๆ
     - ถ้าจะเปิดให้ระบบอื่นเรียกจริง ควรแยกไฟล์ใหม่แล้วใส่ validateApiKey
+    
+GET : http://localhost:3000/api/access/user-accounts?search=admin
 */
 export { GET, POST } from "@/app/api/admin/user-accounts/route";
